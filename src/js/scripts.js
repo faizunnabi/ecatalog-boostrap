b12de0afc944752f5c55b2ebe8db203d98b7b168
faizunnabi <faizunnabi1990@gmail.com>
2017-11-22 12:39:00 +0400

    Third commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,27 @@
 function goBack() {
     window.history.back();
 }
+$(document).ready(function() {
+    var setLang = $('#lanChange');
+    if (localStorage.getItem('lang') == null) {
+        localStorage.setItem('lang', 'EN');
+        setLang.text('AR');
+    } else {
+        if (localStorage.getItem('lang') == 'EN') {
+            setLang.text('AR');
+        } else {
+            setLang.text('EN');
+        }
+    }
+    setLang.click(function() {
+        if (localStorage.getItem('lang') == 'EN') {
+            localStorage.setItem('lang', 'AR');
+            setLang.text('EN');
+            alert('Language changed to Arabic');
+        } else if (localStorage.getItem('lang') == 'AR') {
+            localStorage.setItem('lang', 'EN');
+            setLang.text('AR');
+            alert('Language changed to English');
+        }
+    });
+});
