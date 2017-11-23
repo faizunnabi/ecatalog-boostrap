91e9f6560de2f3ceb5013372997e1c6822b8e925
faizunnabi <faizunnabi1990@gmail.com>
2017-11-23 12:32:24 +0400

    language added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,32 @@
 function goBack() {
     window.history.back();
 }
+
 $(document).ready(function() {
     var setLang = $('#lanChange');
+    var url = window.location.href;
+    //var pagePart = url.split('/');
+    var pageName = url.split('.');
+    console.log(pageName[0]);
     if (localStorage.getItem('lang') == null) {
         localStorage.setItem('lang', 'EN');
         setLang.text('AR');
     } else {
         if (localStorage.getItem('lang') == 'EN') {
             setLang.text('AR');
+
         } else {
             setLang.text('EN');
+
         }
     }
     setLang.click(function() {
         if (localStorage.getItem('lang') == 'EN') {
             localStorage.setItem('lang', 'AR');
-            setLang.text('EN');
-            alert('Language changed to Arabic');
+            window.location.href = pageName[0] + '-ar.html';
         } else if (localStorage.getItem('lang') == 'AR') {
             localStorage.setItem('lang', 'EN');
-            setLang.text('AR');
-            alert('Language changed to English');
+            window.location.href = pageName[0].split('-')[0] + '.html';
         }
     });
 });
