5a48594994d41e8d7dba704f3c305c191fe80793
faizunnabi <faizunnabi1990@gmail.com>
2017-11-23 11:30:58 +0400

    FIFTH COMMIT

---
+++ src/js/lang/AR.js
@@ -0,0 +1,14 @@
+{
+    'on grid': 'داخل الشبكة',
+    'off grid': 'خارج الشبكة',
+    'outdoor lighting': 'إضاءة خارجية',
+    'retrofit led': 'LED إضافات',
+    'air conditioners': 'مكيفات',
+    'water heaters': 'سخانات مياه',
+    'dc products': 'أدوات ومعدات كهربائية',
+    'led fittings': 'LED تركيب لمبات',
+    'full home system': 'أنظمة متكاملة للمنازل',
+    'standalone': 'أنظمة ذاتية',
+    'outdoor lighting': 'إضاءة خارجية',
+    'daylight': 'إضاءة النهار'
+}
